ace644e180e80645b8c5b4ae389b27ce18f8ba79
Alexandr Yermak <ayermak@protonmail.ch>
2025-06-08 01:03:26 +1000

    bump input-method-select

--- Formula/input-method-select.rb
+++ Formula/input-method-select.rb
@@ -3,16 +3,16 @@ class InputMethodSelect < Formula
   homepage "https://github.com/postrockreverb/input-method-select"
   license "MIT"
 
-  version "v1.0.2"
+  version "v1.0.3"
 
   on_arm do
     url "https://github.com/postrockreverb/input-method-select/releases/download/#{version}/dist-arm64.zip"
-    sha256 "5624bde326116a5039298660215c964ff8440c399aeaf11c2bf6d2337b162ac8"
+    sha256 "93c2669ef77ba56b8c8791d707d3736ddee8bcd2a38d96403fbb7a330f5d1711"
   end
 
   on_intel do
     url "https://github.com/postrockreverb/input-method-select/releases/download/#{version}/dist-x86_64.zip"
-    sha256 "dd305b1eb42ccc349d935c845e7c9130467851ab78286921647bcde541ccd81f"
+    sha256 "21026080f16514db2ec50aab1a1898639ee3a19c9123a5c5018fa2b34bf55a01"
   end
 
   def install
